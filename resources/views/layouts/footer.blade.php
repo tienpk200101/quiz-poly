6a74457822480e51f4a5f28036c5ca89ecf05144
tienpk200101 <phungkhactien2001@gmail.com>
2022-06-26 13:45:37 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,43 @@
+<footer class="main-footer">
+  <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 3.2.0
+  </div>
+</footer>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="/template/admin/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="/template/admin/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="/template/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="/template/admin/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="/template/admin/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="/template/admin/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="/template/admin/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="/template/admin/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="/template/admin/plugins/moment/moment.min.js"></script>
+<script src="/template/admin/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="/template/admin/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="/template/admin/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="/template/admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/template/admin/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/template/admin/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="/template/admin/dist/js/pages/dashboard.js"></script>
